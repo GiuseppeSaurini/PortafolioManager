d404da89b30e10e4bcafe6e1c834400200003430
GiuseppeSaurini <69056857+GiuseppeSaurini@users.noreply.github.com>
2021-07-05 10:28:29 -0400

    Update ImportData.py

--- ImportData.py
+++ ImportData.py
@@ -23,37 +23,67 @@ def importData(isin=None,table='instrumentos',fecha_base=''):
     
     #Definir que tabla se busca
     query=url_DWH+table+key
+    query=query+('&isin='+isin if isin!=None else '')
     
-    #Carga de filtros
-    query=query+('&isin='+isin if isin!=None else '') 
-    query=query+('&fecha_vencimiento_mt='+fecha_base)
-   
-    #importar los datos
-    data=json.loads(requests.get(query).text)
-    df=pd.json_normalize(data,sep='/')
+    if(table=='instrumentos'):
+        #Carga de filtros
+        query=query+('&fecha_vencimiento_mt='+fecha_base)
+        
+        #importar los datos
+        data=json.loads(requests.get(query).text)
+        df=pd.json_normalize(data,sep='/')
+        
+        #eliminar datos None
+        df=df.replace('None','')
+        #definir el la columna de index
+        df=df.set_index('isin')
     
-    #ajustes de datos
-    df=df.replace('None','')
-    for c in df.columns:
-        if(c[:12]=='instrumento/'):
-            df=df.rename(columns={c:c[12:]})
+    elif(table=='operaciones'):
+        query=query+('&fecha_operacion_from='+fecha_base)
+        #importar los datos
+        data=json.loads(requests.get(query).text)
+        df=pd.json_normalize(data,sep='/')
+        
+        #eliminar datos None
+        df=df.replace('None','')
+        
+        for c in df.columns:
+            if(c[:12]=='instrumento/'):
+                df=df.rename(columns={c:c[12:]})
+        
+        df['fecha_operacion']=pd.to_datetime(df['fecha_operacion'])
+        #definir el la columna de index
+        df=df.set_index('id')
+       
+    elif (table=='flujos'):
+        query=query+('&fecha_operacion_from='+fecha_base)
+        #importar los datos
+        data=json.loads(requests.get(query).text)
+        df=pd.json_normalize(data,sep='/')
+        
+        #eliminar datos None
+        df=df.replace('None','')
+        
+        for c in df.columns:
+            if(c[:12]=='instrumento/'):
+                df=df.rename(columns={c:c[12:]})
+        
+        df['fecha']=pd.to_datetime(df['fecha'])
+        #definir el la columna de index
+        df=df.set_index('id')
+    
     df['fecha_colocacion']=pd.to_datetime(df['fecha_colocacion'])
     df['fecha_vencimiento']=pd.to_datetime(df['fecha_vencimiento'])
     df['valor_nominal']=pd.to_numeric(df['valor_nominal'])
     
-    if(table=='operaciones'):
-        df['fecha_operacion']=pd.to_datetime(df['fecha_operacion'])
-    elif (table=='flujos'):
-        df['fecha']=pd.to_datetime(df['fecha'])
-    
     for row in df.itertuples():
-        df.loc[row.Index,'simbolo_emisor']=row.isin[2:5]
-    
-    #definir el la columna de index
-    if(table=='instrumentos'):
-        df=df.set_index('isin')
-    else:
-        df=df.set_index('id')
+        if(df.loc[row.Index,'simbolo_emisor']==''):    
+            df.loc[row.Index,'simbolo_emisor']=row.isin[2:5]
+        if(row.moneda!='pyg' or row.moneda!='usd'):
+            if(row.moneda[0]=='D'):
+                df.loc[row.Index,'moneda']=='usd'
+            else:
+                df.loc[row.Index,'moneda']=='pyg'
         
     return df
 
