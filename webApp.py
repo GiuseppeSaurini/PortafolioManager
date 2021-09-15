0673ae33282557e0fa5b799c6fce5fe775307cae
GiuseppeSaurini <69056857+GiuseppeSaurini@users.noreply.github.com>
2021-09-15 18:37:19 -0400

    Update webApp.py

--- webApp.py
+++ webApp.py
@@ -16,6 +16,7 @@ from Mercados import Mercado
 from ImportData import importData
 from datetime import datetime,timedelta,date
 
+st.title('Valoración de Bono')
 
 #Definision de instrumentos
 
@@ -25,8 +26,6 @@ fecha_str=str(fecha.year)+'-'+str(fecha.month)+'-'+str(fecha.day)
 emisiones = importData(table='instrumentos',fecha_base=fecha_str)
 emisiones['fecha_vencimiento']=pd.to_datetime(emisiones['fecha_vencimiento'])
 emisiones=emisiones[~(emisiones['fecha_vencimiento'].isna())]
-
-#emisiones=emisiones[emisiones['fecha_vencimiento']>datetime.today()]
 
 
 #Select section
@@ -39,25 +38,34 @@ flujos=importData(isin,'flujos').sort_values(by='fecha')
 
 bono=Mercado(flujos).getBond(isin) 
 
+#Infomacion del instrumento seleccionado
+st.write('Datos del instrumento seleccionado')
+st.table(bono.info)
+
+#Datos de cotizacion del Bono
 fecha_cotizacion=st.sidebar.date_input('Selecciones la fecha de valoracion:',
                                        value=date.today())
 
 fecha_cotizacion=pd.to_datetime(fecha_cotizacion)
 
-cotizacion=st.sidebar.selectbox(
+metodo_cotizacion=st.sidebar.selectbox(
                                     "Selecciona metodo de cotizacion",
                                         ['Precio Dirty',
                                         'Precio Clean',
-                                        'Rendimimiento']
+                                        'Precio Base',
+                                        'Rendimimiento(TIR)']
                                     )
 
-if(cotizacion=='Precio Dirty'):
+if(metodo_cotizacion=='Precio Dirty'):
     price= st.sidebar.text_input('Precio Dirty:',value='100.00')
     price=pd.to_numeric(price)
-    rendimiento=bono.rendimiento(fecha_cotizacion,
-                                (price)/100*bono.info['ValorNominal'])
 
-elif(cotizacion=='Precio Clean'):
+    valorActualNeto=(price)/100*bono.info['ValorNominal']
+
+    rendimiento=bono.rendimiento(fecha_cotizacion,
+                                valorActualNeto)
+
+elif(metodo_cotizacion=='Precio Clean'):
     price= st.sidebar.text_input('Precio Clean:',value='100.00')
     price=pd.to_numeric(price)
     
@@ -65,21 +73,26 @@ elif(cotizacion=='Precio Clean'):
     
     interesCorrido=bono.info['TasaCupon']/365*dias_Corridos*100
     
-    price_clean=(price+interesCorrido)/100*bono.info['ValorNominal']
-    
+    valor_clean=(price+interesCorrido)/100*bono.info['ValorNominal']
     
     rendimiento=bono.rendimiento(fecha_cotizacion,
-                                 price_clean)
+                                 valor_clean)
 
-elif(cotizacion=='Rendimimiento'):
+elif(metodo_cotizacion=='Precio Base'):
+    price_base= st.sidebar.text_input('Precio Base:',value='100.00')
+    price_base=pd.to_numeric(price_base)
+    dias_Corridos=bono.diasCorridos(fecha_cotizacion)
+    nueva_fecha_cotizacion=fecha_cotizacion-timedelta(days=dias_Corridos)
+    valor_base=price_base/100*bono.info['ValorNominal']
+    rendimiento=bono.rendimiento(nueva_fecha_cotizacion,valor_base)
+    
+
+elif(metodo_cotizacion=='Rendimimiento(TIR)'):
     price=st.sidebar.text_input('Rendimimiento',value='0.100')
     price=pd.to_numeric(price)
     rendimiento=price
 
-st.title('Valoración de Bono')
 
-st.write('Datos del instrumento seleccionado')
-st.table(bono.info)
 
 st.write('Datos valoración')
 st.table(bono.datosValor(rendimiento,fecha_cotizacion))
