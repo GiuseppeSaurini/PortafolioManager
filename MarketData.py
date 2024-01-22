0328cb762c609831e02c7e8992049112e66324d9
GiuseppeSaurini <gsaurini@gmail.com>
2024-01-22 09:38:44 -0300

    Update MarketData.py

--- MarketData.py
+++ MarketData.py
@@ -9,18 +9,20 @@ Created on Fri Apr 30 16:03:23 2021
 
 # Cargar las librería de python data analysis
 import pandas as pd 
-import numpy as np
-from datetime import datetime, timedelta
+#import numpy as np
+#from datetime import datetime, timedelta
 import os
 import requests
 import json
 
-class MarketDataAPI():
+MD_API_KEY=os.environ.get('MD_API_KEY')
+
+class MarketDataAPI:
     
-    def get_instrumentos(isin='',fecha_base=''):
+    def get_instrumento(isin='',fecha_base=''):
         #API Datawharehousing url y contraseña
         url_DWH='https://data.marketdata.com.py/api/v1/'
-        key='?api_key='+os.environ.get('MD_API_KEY')
+        key='?api_key='+MD_API_KEY
         
         #Definir que tabla se busca
         query=url_DWH+'instrumentos/list'+key
@@ -131,7 +133,7 @@ class MarketDataAPI():
             
         return df
     
-    def get_flujos(isin=''):
+    def get_flujo(isin=''):
         #API Datawharehousing url y contraseña
         
         url_DWH='https://data.marketdata.com.py/api/v1/'
@@ -170,72 +172,28 @@ class MarketDataAPI():
         
         return df
     
-"""
-Guardado de codigo inicial de importacion de datos
- # def importData(isin='',table='instrumentos/list',fecha_base=''):
-    #     #API Datawharehousing url y contraseña
+    def get_flujos_from_list(self,list_isin):
+        """
+        Funcion para importar varios flujos de un
+        listado de instrumentos solo agregando el 
+        codigo isin a la lista.
+        El resultado es un DataFrmae con los datos 
+        del los flujos de instrumentos seleccionados.
+        En el caso que no tengan flujo se imprime el isin
         
-    #     url_DWH='https://data.marketdata.com.py/api/v1/'
-    #     key='?api_key='+MD_API_KEY
-        
-    #     #Definir que tabla se busca
-    #     query=url_DWH+table+key
-        
-    #     #Filtrar por isin    
-    #     query=query+'&isin='+isin
-        
-    #     #Carga de filtros fecha
-    #     query=query+('&fecha_vencimiento_mt='+fecha_base)
-    #     if(table=='operaciones'):
-    #         query=query+('&fecha_operacion_from='+fecha_base)    
-        
-    #     #importar los datos
-    #     data=json.loads(requests.get(query).text)
-    #     df=pd.json_normalize(data,sep='/')
-        
-    #     #eliminar datos None
-    #     df=df.replace('None','')
-          
-    #     #Desglosar instrumento
-    #     for c in df.columns:
-    #         if(c[:12]=='instrumento/'):
-    #             df=df.rename(columns={c:c[12:]})
-    #     #Desglose simbolo_emisor
-    #     for c in df.columns:
-    #         if(c[:15]=='simbolo_emisor/'):
-    #             df=df.rename(columns={c:c[15:]})
-              
-    #     #Ajustes condicionales
-    #     if(table=='instrumentos/list'):
-    #         #definir el la columna de index
-    #         df=df.set_index('isin')
-            
-    #     elif(table=='operaciones'):
-    #         df['fecha_operacion']=pd.to_datetime(df['fecha_operacion'])
-            
-           
-    #     elif (table=='flujos'):
-    #         df['fecha']=pd.to_datetime(df['fecha'])
-     
-        
-    #     df['fecha_colocacion']=pd.to_datetime(df['fecha_colocacion'])
-    #     df['fecha_vencimiento']=pd.to_datetime(df['fecha_vencimiento'])
-    #     df['valor_nominal']=pd.to_numeric(df['valor_nominal'])
-        
-    #     for row in df.itertuples():
-    #         # if(table!='instrumentos'):
-    #         #     if(df.loc[row.Index,'simbolo_emisor']==''):    
-    #         #         df.loc[row.Index,'simbolo_emisor']=row.isin[2:5]
-                    
-    #         if(row.moneda!='pyg' or row.moneda!='usd'):
-    #             try:
-    #                 if(row.moneda[0]==''):
-    #                     df.loc[row.Index,'moneda']=='usd'
-    #                 else:
-    #                     df.loc[row.Index,'moneda']=='pyg'
-    #             except:
-    #                 pass
-                
-            
-    #     return df
-"""   
+        """
+        """
+        Este metodo es un instance method por lo que 
+        para utilizar la funcion primero se debe activar 
+        a la clase:
+        md=MarketDataAPI()
+        md.get_flujos_from_list(list_isin)
+        """
+        flujos=pd.DataFrame()
+        for isin in list_isin:
+            try:
+                flujo_bono=MarketDataAPI.get_flujo(isin)  
+                flujos=pd.concat([flujos,flujo_bono])
+            except:
+                print('no tiene flujo',isin)
+        return flujos
